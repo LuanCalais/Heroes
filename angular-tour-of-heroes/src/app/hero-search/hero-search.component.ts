44acbc914ab4040b014ba0ef249f337ee90add5f
LuanCalais <luancalais@gmail.com>
2022-07-19 18:14:44 -0300

    funcoes para funcionamento de async pipe em hero-search component ts definidas

--- angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { from, Observable, Subject } from 'rxjs';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+/* 
+ debounceTime - define um tempo antes de executar algo
+ distinctUntilChanged - apenas considera entradas distintas
+ switchMap - altera para um novo do mesmo tipo no fim da execução
+*/
+
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
@@ -7,9 +22,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSearchComponent implements OnInit {
 
-  constructor() { }
+  // Declaração de Async pipe 
+  heroes$!: Observable<Hero[]>
+
+  // Lista observable que recebe os termos em uma lista de string
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) { }
+
+  // Push do termo da pesquisa dentro da ferramenta do observable
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+
+    this.heroes$ = this.searchTerms.pipe(
+      // Espera 300 milisegundos depois de cada entrada key antes de considerar o termo
+      debounceTime(300),
+
+      // igora o termpo de entrada se ele for igual ao ultimo
+      distinctUntilChanged(),
+
+      // no fim, altera para outro novo search sempre que o termo mudar
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+
+    )
+
   }
 
 }
